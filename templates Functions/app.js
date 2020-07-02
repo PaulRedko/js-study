7fe1db216261e8dec8354ea31befc31ab2c75189
PaulRed <pavlo.redko@gmail.com>
2020-07-02 17:16:10 +0300

    added first task on password validation

--- templates Functions/app.js
+++ templates Functions/app.js
@@ -39,3 +39,31 @@
 //     return x/y
 // }
 
+
+// Write a isValidPassword function
+// It accepts 2 arguments: password and username
+// Password must:
+//	- be at least 8 characters
+//  - cannot contain spaces
+//  - cannot contain the username
+// If all requirements are met, return true.
+//Otherwise: false
+
+// isValidPassword('89Fjj1nms', 'dogLuvr');  //true
+// isValidPassword('dogLuvr123!', 'dogLuvr') //false
+// isValidPassword('hello1', 'dogLuvr') //false
+
+//declare
+function isValidPassword(pass, userName) {
+    // if(pass.length < 8) {return false};
+    // if(pass.indexOf(' ') !== -1) {return false};
+    // if(pass.indexOf(userName) !== -1){ return false}
+    // return true;
+    // }
+
+    if((pass.length < 8) || (pass.indexOf(' ') !== -1) || (pass.indexOf(userName)) 
+    {return false};
+    return true;
+    }
+//run
+isValidPassword('PbaPas!321','PbaPas!321');
