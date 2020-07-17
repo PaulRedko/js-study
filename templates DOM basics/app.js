b76e5c62163697257d4c2b7631c986134174e0f3
PaulRed <pavlo.redko@gmail.com>
2020-07-17 19:29:08 +0300

    current DOM commit

--- templates DOM basics/app.js
+++ templates DOM basics/app.js
@@ -1,4 +1,12 @@
 const liS = document.querySelectorAll('li');
-for (let li of liS) {
-  li.innerHTML = 'WE ARE THE <b>CHAMPIONS!</b>';
-}
+const colors = ['red', 'yellow', 'blue'];
+liS.forEach((li, i) => {
+  const color = colors[i];
+  li.style.color = color;
+});
+
+const todo = document.querySelector('#todos .todo');
+
+// todo.style.color = 'gray';
+// todo.style.opacity = '50%';
+// todo.style.textDecoration = 'line-through';
