8c191a293d39eb96db73ecbfe69371c8a483abea
PaulRed <pavlo.redko@gmail.com>
2020-07-21 12:22:39 +0300

    dom current

--- templates DOM basics/app.js
+++ templates DOM basics/app.js
@@ -12,7 +12,7 @@
 // todo.style.textDecoration = 'line-through';
 
 //Create element
-// document.createElement('h2'); // created element
+document.createElement('h2'); // created element
 const newH2 = document.createElement('h2'); // made it as a var
 // add content
 newH2.textContent = 'My best day and week in life';
@@ -22,11 +22,18 @@ newH2.classList.add('special');
 const frm = document.querySelector('form');
 frm.appendChild(newH2);
 
-const imag = document.createElement('img');
-// https://images.unsplash.com/photo-1529245814698-dd66c442bfef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80
+// const imag = document.createElement('img');
+// // https://images.unsplash.com/photo-1529245814698-dd66c442bfef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80
 
-imag.src =
-  'https://images.unsplash.com/photo-1529245814698-dd66c442bfef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80';
+// imag.src =
+//   'https://images.unsplash.com/photo-1529245814698-dd66c442bfef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80';
 
-const ul = document.querySelector('ul');
-ul.prepend(imag);
+// const ul = document.querySelector('ul');
+// ul.prepend(imag);
+
+// function nameAge(name, age) {
+//   document.write(`<br> Hello, ${name}. Are you ${age}? <br>`);
+// }
+// nameAge('Paul', 38);
+// nameAge('Sofia', 5);
+// nameAge('Inna', 47);
