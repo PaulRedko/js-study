040bf8a57f57307370c7c67fd1e2a4290117851f
PaulRed <pavlo.redko@gmail.com>
2020-07-24 12:16:32 +0300

    color picking app

--- templates DOM events/multiple events per element/app.js
+++ templates DOM events/multiple events per element/app.js
@@ -9,10 +9,16 @@ const colors = [
   'teal',
 ];
 
-const boxes = document.querySelector('#boxes');
+const changeColor = function () {
+  const h1 = document.querySelector('h1');
+  h1.style.color = this.style.backgroundColor;
+};
+const container = document.querySelector('#boxes');
+
 for (let color of colors) {
   const box = document.createElement('div');
   box.style.backgroundColor = color;
   box.classList.add('box');
-  document.body.append(box);
+  container.appendChild(box);
+  box.addEventListener('click', changeColor);
 }
