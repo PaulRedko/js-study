fca1481acea1842c50f0763317c703f33b221b96
PaulRed <pavlo.redko@gmail.com>
2020-08-13 12:32:05 +0300

    todo app

--- training_arrays-todolist/training.js
+++ training_arrays-todolist/training.js
@@ -4,28 +4,44 @@
 // quit - stops the app
 
 let answer = prompt('What would you like to do?');
-let toDoList = [];
+let toDoList = ['Make the bed'];
 // let toDo = function () {
 // }
 // toDo();
 
 while (answer !== 'quit') {
   if (answer === 'list') {
-    toDoList.forEach(function (toDo) {
-      console.log('************');
-      console.log(toDo);
-      console.log('************');
-    });
+    console.log('************');
+    listTodos();
   } else if (answer === 'new') {
-    toDoList.push(prompt('What task to add?'));
+    addTodo();
+  } else if (answer === 'delete') {
+    // ask for index
+    deleteTodo();
   }
   answer = prompt('What would you like to do?');
 }
 console.log('Ok, you have quit the app!');
 
+function listTodos() {
+  toDoList.forEach(function (toDo, index) {
+    console.log(`${index}: ${toDo}`);
+  });
+  console.log('************');
+}
+function addTodo() {
+  toDoList.push(prompt('What task to add?'));
+}
+function deleteTodo() {
+  let index = prompt('What do you want to delete?');
+  toDoList.splice(index, 1);
+  console.log('*****The List after delete');
+  listTodos();
+}
+
 // FOREACH LOOP TASKS
 // let colors = ['red', 'green', 'blue', 'brown', 'black'];
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let numbers = [1, 2, 3, 4,  5, 6, 7, 8, 9, 10];
 
 // numbers.forEach(function (number) {
 //   if (number % 2 === 0) {
